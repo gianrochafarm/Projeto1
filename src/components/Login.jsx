a5c1fefdd1ce642220ff5d60cbf24903be88daf2
Felippe Guedes <felippec.guedes@gmail.com>
2023-01-09 12:28:39 -0300

    tela de login

---
+++ src/components/Login.jsx
@@ -0,0 +1,37 @@
+import '../home/global.css'
+import styles from './Login.module.css'
+
+export function Login() {
+
+    return (
+        <form className={ styles.form }>
+
+
+            <div className={ styles.login}>
+                <h1>Login</h1>
+            </div>
+
+            <div className={ styles.input }>
+                <p>Seu e-mail:</p>
+                <input type="email" name='Seu email:' size={25} />
+            </div>
+
+            <div className={ styles.input }>
+                <p>Sua senha:</p>
+                <input  type="password" name='Sua senha:' size={25} />
+            </div>
+
+            <div className={ styles.checkbox }>
+                <input type="checkbox" />
+                Manter-me logado.
+            </div>
+
+            <div className={ styles.button }>
+                <input type="submit" value={"Logar"} />
+                
+            </div>
+
+        
+        </form>
+        )
+}
---
+++ src/home/App.jsx
@@ -0,0 +1,18 @@
+import { Header } from '../components/Header'
+import { Login } from '../components/Login'
+import './global.css'
+import styles from './App.module.css'
+
+function App() {
+
+  return (
+    <div className={ styles.app }>
+<Header />
+
+<Login />  
+    
+    </div>
+  )
+}
+
+export default App
