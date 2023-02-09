d11ee0eb58cf2accefc11a03a2b6cdc5fb851105
Felippe Guedes <felippec.guedes@gmail.com>
2023-02-09 08:25:55 -0300

    Validação de senha com search

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,42 @@
+import { useState } from "react";
 import "../home/global.css";
 import styles from "./Login.module.css";
 
-export function Login() {	
-	
-	
-	
 
+export function Login() {	
+
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	const handleChangeEmail = (e) => {
+		setEmail(e);
+	}
+
+	const handleChangePassword = (e) => {
+		setPassword(e);
+	}
+
+	//  console.log(password);
+	//  console.log(password.search(/[A-Z]/))
+	//  console.log(password.search(/[a-z]/))
+	//  console.log(password.search(/[0-9]/))
+	//  console.log(password.search(/[\W]/))
+	 
+
+	const aplyForm = () => {
+		if (email.length === 0) return alert('Preencha o campo email.');
+		if (!email.includes('@')) return alert('Email fora dos padrões.');
+		if (password.length === 0) return alert('Preencha o campo senha.');
+		if (password.search(/[A-Z]/) < 0) return alert('Sua senha precisa de ao menos uma letra maiúscula.');
+		if (password.search(/[a-z]/) < 0) return alert('Sua senha precisa de ao menos uma letra minúscula.');
+		if (password.search(/[0-9]/) < 0) return alert('Sua senha precisa de ao menos um número.');
+		if (password.search(/[\W]/) < 0) return alert('Sua senha precisa de ao menos um caracter especial.');
+		return alert('OK');
+	}
 
 	return (
 
-		<form className={ styles.form }>
+		<form className={ styles.form } onSubmit={aplyForm}>
 
 			<div className={ styles.login}>
 				<h1>Login</h1>
@@ -17,14 +44,15 @@ export function Login() {
 
 			<div className={ styles.email }>
 				<p>Seu e-mail:</p>
+				
 				<input 
-					type="email" 
 					name='Seu email:' 
 					size={25}
-					
-								
+					value={email}
+					onChange={(event)=> handleChangeEmail(event.target.value)}
 					
 				/>
+
 			</div>
 
 			<div className={ styles.senha }>
@@ -33,6 +61,8 @@ export function Login() {
 					type="password" 
 					name='Sua senha:' 
 					size={25}
+					value={password}
+					onChange={(event)=> handleChangePassword(event.target.value)}
 									
 				/>
 			</div>
