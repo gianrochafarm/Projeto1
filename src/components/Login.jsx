1092c0871b5f7f3b02b643318f0fd27828db6aae
Felippe Guedes <felippec.guedes@gmail.com>
2023-02-11 12:32:28 -0300

    Span de mensagem de erro

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,20 +3,25 @@ import "../home/global.css";
 import styles from "./Login.module.css";
 
 
-export function Login() {	
-
-	
+export function Login() {
 
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
+	const [errorEmail, setErrorEmail] = useState('');
+	const [errorPassword, setErrorPassword] = useState('');
+
+
 
 	const handleChangeEmail = (e) => {
 		setEmail(e);
-	}
+		setErrorEmail('');
+	};
 
 	const handleChangePassword = (e) => {
 		setPassword(e);
-	}
+	};
+
+
 	
 	//  console.log(password);
 	//  console.log(password.search(/[A-Z]/))
@@ -25,16 +30,17 @@ export function Login() {
 	//  console.log(password.search(/[\W]/))
 	 
 
-	const aplyForm = () => {
-		if (email.length === 0) return alert('Preencha o campo email.');
-		if (!email.includes('@')) return alert('Email fora dos padrões.');
-		if (password.length === 0) return alert('Preencha o campo senha.');
-		if (password.search(/[A-Z]/) < 0) return alert('Sua senha precisa de ao menos uma letra maiúscula.');
-		if (password.search(/[a-z]/) < 0) return alert('Sua senha precisa de ao menos uma letra minúscula.');
-		if (password.search(/[0-9]/) < 0) return alert('Sua senha precisa de ao menos um número.');
-		if (password.search(/[\W]/) < 0) return alert('Sua senha precisa de ao menos um caracter especial.');
+	const aplyForm = (e) => {
+		e.preventDefault('');
+		if (email.length === 0) return setErrorEmail('Por favor, insira seu email.');
+		if (!email.includes('@')) return setErrorEmail('Email fora dos padrões.');
+		if (password.length === 0) return setErrorPassword('Preencha o campo senha.');
+		if (password.search(/[A-Z]/) < 0) return setErrorPassword('Sua senha precisa de ao menos uma letra maiúscula.');
+		if (password.search(/[a-z]/) < 0) return setErrorPassword('Sua senha precisa de ao menos uma letra minúscula.');
+		if (password.search(/[0-9]/) < 0) return setErrorPassword('Sua senha precisa de ao menos um número.');
+		if (password.search(/[\W]/) < 0) return setErrorPassword('Sua senha precisa de ao menos um caracter especial.');
 		return localStorage.setItem('Email',email);
-	}
+	};
 
 	return (
 
@@ -44,29 +50,63 @@ export function Login() {
 				<h1>Login</h1>
 			</div>
 
-			<div className={ styles.email }>
+			{errorEmail ? <div className={ styles.errorEmail }>
 				<p>Seu e-mail:</p>
-				
+
 				<input 
 					name='Seu email:' 
 					size={25}
 					value={email}
-					onChange={(event)=> handleChangeEmail(event.target.value)}
-					
+					onChange={(event)=> handleChangeEmail(event.target.value)}															
 				/>
+			</div> 
 
+				:
+
+			<div className={ styles.email }>
+				<p>Seu e-mail:</p>
+
+				<input 
+					name='Seu email:' 
+					size={25}
+					value={email}
+					onChange={(event)=> handleChangeEmail(event.target.value)}															
+				/>
+			</div> }
+			
+			
+			<div>
+			{errorEmail && <span className={styles.errorAlert}>{errorEmail}</span>}
 			</div>
 
-			<div className={ styles.senha }>
+			{errorPassword ?
+			<div className={ styles.errorPassWord }>
 				<p>Sua senha:</p>
+				
 				<input  
 					type="password" 
 					name='Sua senha:' 
 					size={25}
 					value={password}
-					onChange={(event)=> handleChangePassword(event.target.value)}
-									
+					onChange={(event)=> handleChangePassword(event.target.value)}									
 				/>
+			</div> : 
+			
+			<div className={ styles.senha }>
+				<p>Sua senha:</p>
+				
+				<input  
+					type="password" 
+					name='Sua senha:' 
+					size={25}
+					value={password}
+					onChange={(event)=> handleChangePassword(event.target.value)}									
+				/>
+			</div>
+			}
+
+			<div>
+			{errorPassword && <span className={styles.errorAlert}>{errorPassword}</span>}
 			</div>
 
 			<div className={ styles.checkbox }>
