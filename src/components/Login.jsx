00127ac1d0b37152ad295db057468071f90ed702
Felippe Guedes <felippec.guedes@gmail.com>
2023-02-14 23:21:51 -0300

    tela de cadastro

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -9,6 +9,10 @@ export function Login() {
 	const [password, setPassword] = useState('');
 	const [errorEmail, setErrorEmail] = useState('');
 	const [errorPassword, setErrorPassword] = useState('');
+	const [register, setRegister] = useState(false);
+	const [confirmPass, setConfirmPass] = useState('');
+	const [createPass, setCreatePass] = useState('');
+	const [errorConfirmPass, setErrorConfirmPass ] = useState('');
 
 
 
@@ -19,40 +23,70 @@ export function Login() {
 
 	const handleChangePassword = (e) => {
 		setPassword(e);
+		setErrorPassword('');
 	};
 
+	const handleConfirmPassword = (e) => {
+		setConfirmPass(e);
+		setErrorPassword('');
+	}
 
-	
-	//  console.log(password);
-	//  console.log(password.search(/[A-Z]/))
-	//  console.log(password.search(/[a-z]/))
-	//  console.log(password.search(/[0-9]/))
-	//  console.log(password.search(/[\W]/))
+	 const handleCreatePassword = (e) => {
+		setCreatePass(e);
+		setErrorPassword('');
+	 }
+
+	const toggle = (e) => {
+		e.preventDefault();
+		setRegister(!register)
+		setErrorPassword('');
+		setErrorEmail('');
+		setEmail('');
+		setPassword('');
+		setConfirmPass('');
+		setCreatePass ('');	
+		setErrorConfirmPass('');
+	}
 	 
 
-	const aplyForm = (e) => {
+	const aplyFormRegister = (e) => {
 		e.preventDefault('');
 		if (email.length === 0) return setErrorEmail('Por favor, insira seu email.');
 		if (!email.includes('@')) return setErrorEmail('Email fora dos padrões.');
-		if (password.length === 0) return setErrorPassword('Por favor, insira sua senha.');
-		if (password.search(/[A-Z]/) < 0) return setErrorPassword('Sua senha precisa de ao menos uma letra maiúscula.');
-		if (password.search(/[a-z]/) < 0) return setErrorPassword('Sua senha precisa de ao menos uma letra minúscula.');
-		if (password.search(/[0-9]/) < 0) return setErrorPassword('Sua senha precisa de ao menos um número.');
-		if (password.search(/[\W]/) < 0) return setErrorPassword('Sua senha precisa de ao menos um caracter especial.');
+		if (createPass.length === 0) return setErrorPassword('Por favor, insira sua senha.');
+		if (createPass.length === 0) return setErrorPassword('Por favor, insira sua senha.');
+		if (createPass.search(/[A-Z]/) < 0) return setErrorPassword('Sua senha precisa de ao menos uma letra maiúscula.');
+		if (createPass.search(/[a-z]/) < 0) return setErrorPassword('Sua senha precisa de ao menos uma letra minúscula.');
+		if (createPass.search(/[0-9]/) < 0) return setErrorPassword('Sua senha precisa de ao menos um número.');
+		if (createPass.search(/[\W]/) < 0) return setErrorPassword('Sua senha precisa de ao menos um caracter especial.');
+		if (confirmPass.length === 0) return setErrorConfirmPass('Por favor, confirme sua senha.');
+		if (confirmPass !== createPass) return setErrorConfirmPass('Senhas diferentes');
 		window.location.reload();
 		return localStorage.setItem('Email',email);		
 
 	};
 
+	const aplyFormLogin = (e) => {
+		e.preventDefault('');
+		if (email.length === 0) return setErrorEmail('Por favor, insira seu email.');
+		if (!email.includes('@')) return setErrorEmail('Email fora dos padrões.');
+		if (password.length === 0) return setErrorPassword('Por favor, insira sua senha.');
+		window.location.reload();
+		return localStorage.setItem('Email',email);		
+
+	};
+	
+	if(!register){
+
 	return (
 
-		<form className={ styles.form } onSubmit={aplyForm} autoComplete='off'>
+		<form className={ styles.form } onSubmit={aplyFormLogin} autoComplete='off'>
 
 			<div className={ styles.login}>
 				<h1>Login</h1>
 			</div>
 
-			{errorEmail ? <div className={ styles.errorEmail }> 
+			 <div className={errorEmail ? styles.errorEmail : styles.email }> 
 				<p>Seu e-mail:</p>
 
 				<input 
@@ -63,38 +97,12 @@ export function Login() {
 				/>
 			</div> 
 
-				:
-
-			<div className={ styles.email }>
-				<p>Seu e-mail:</p>
-
-				<input 
-					name='Seu email:' 
-					size={25}
-					value={email}
-					onChange={(event)=> handleChangeEmail(event.target.value)}															
-				/>
-			</div> }
-			
-			
 			<div>
 			{errorEmail && <span className={styles.errorAlert}>{errorEmail}</span>}
 			</div>
 
-			{errorPassword ?
-			<div className={ styles.errorPassWord }>
-				<p>Sua senha:</p>
-				
-				<input  
-					type="password" 
-					name='Sua senha:' 
-					size={25}
-					value={password}
-					onChange={(event)=> handleChangePassword(event.target.value)}									
-				/>
-			</div> : 
 			
-			<div className={ styles.password }>
+			<div className={errorPassword ? styles.errorPassWord : styles.password}>
 				<p>Sua senha:</p>
 				
 				<input  
@@ -104,8 +112,7 @@ export function Login() {
 					value={password}
 					onChange={(event)=> handleChangePassword(event.target.value)}									
 				/>
-			</div>
-			}
+			</div> 
 
 			<div>
 			{errorPassword && <span className={styles.errorAlert}>{errorPassword}</span>}
@@ -117,7 +124,7 @@ export function Login() {
 			</div>
 
 			<div className={ styles.changePassword }>				
-				<a href=""> Cadastre-se.</a>
+				<a onClick={toggle} href=""> Cadastre-se.</a>
 			</div>
 
 			<div className={ styles.button } >
@@ -131,5 +138,80 @@ export function Login() {
 
         
 		</form>
-	);
+		
+	);}
+
+	return (
+	<form className={ styles.form } onSubmit={aplyFormRegister} autoComplete='off'>
+
+	<div className={ styles.login}>
+		<h1>Cadastro</h1>
+	</div>
+
+	 <div className={errorEmail ? styles.errorEmail : styles.email }> 
+		<p>Seu e-mail:</p>
+
+		<input 
+			name='Seu email:' 
+			size={25}
+			value={email}
+			onChange={(event)=> handleChangeEmail(event.target.value)}															
+		/>
+	</div> 
+
+	<div>
+	{errorEmail && <span className={styles.errorAlert}>{errorEmail}</span>}
+	</div>
+
+	
+	<div className={errorPassword ? styles.errorPassWord : styles.password}>
+		<p>Digite uma senha:</p>
+		
+		<input  
+			type="password" 
+			name='Sua senha:' 
+			size={25}
+			value={createPass}
+			onChange={(event)=> handleCreatePassword(event.target.value)}									
+		/>
+	</div> 
+
+	<div>
+	{errorPassword && <span className={styles.errorAlert}>{errorPassword}</span>}
+	</div>
+
+	<div className={errorConfirmPass ? styles.errorPassWord : styles.password}>
+		<p>Confirme sua senha:</p>
+		
+		<input  
+			type="password" 
+			name='Sua senha:' 
+			size={25}
+			value={confirmPass}
+			onChange={(event)=> handleConfirmPassword(event.target.value)}									
+		/>
+	</div> 
+
+	<div>
+	{errorConfirmPass && <span className={styles.errorAlert}>{errorConfirmPass}</span>}
+	</div>
+
+
+	<div className={ styles.changePassword }>				
+		<a onClick={toggle} href="">Sou cadastrado.</a>
+	</div>
+
+	<div className={ styles.button } >
+		<input 
+			type="submit" 
+			value={"Registrar"}
+			
+
+		/>                
+	</div>
+
+
+</form>)
+		
+	
 }
